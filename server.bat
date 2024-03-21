c54aac0b4c135bb955ce3b7b3cce5c204dceff03
miniNemiku <155912382+miniNemiku@users.noreply.github.com>
2024-03-21 11:38:40 +0200

    Update server.bat

--- server.bat
+++ server.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Change directory
-cd C:\Users\nemne\Downloads\SPL\Assignment3\Assignment3\server 
+cd <"Directory to your /server">
 
 rem Run Maven compile and execute Java main class
 mvn compile && mvn exec:java -Dexec.mainClass=bgu.spl.net.impl.tftp.TftpServer -Dexec.args=7777
